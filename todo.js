46fa9c8d172efc185ed18de148fc7a530557410b
artishirsath <artishirsath7@gmail.com>
2023-07-26 21:36:33 +0530

    done

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 function addtask(){
     document.getElementById('popup').style.display='block';
-    document.getElementById('NoItems').style.display='none';
     document.getElementById('mainContainer').style.filter= 'blur(8px)';
+    document.getElementById('NoItems').style.display='none';
 }
 function CloseCountry(){
   document.getElementById('popUpForCountry').style.display='none';
@@ -56,7 +56,7 @@ function AddNewTask(){
 
   newCardInParent.style.position='relative';
 
-  // style for heading
+  // realted to heading that is added 
   cardHeading.style.textAlign='center';
   cardHeading.style.color="red";
   cardHeading.style.font="Monospace";
@@ -64,7 +64,7 @@ function AddNewTask(){
   cardHeading.style.textShadow="2px 2px grey";
 
 
-  // style for delete and plus buton 
+  // related to delete and plus buton 
 
   deleteButtonInsideTask.style.width='15%';
   deleteButtonInsideTask.style.height='10%';
@@ -72,8 +72,8 @@ function AddNewTask(){
   deleteButtonInsideTask.style.left='10px';
   deleteButtonInsideTask.style.position='absolute';
   deleteButtonInsideTask.style.bottom='10px';
- //deleteButtonInsideTask.innerHTML=document.getElementById('img1');
- deleteButtonInsideTask.style.color='aqua';
+ // deleteButtonInsideTask.style.backgroundImage="url('unnamed.png')";
+  deleteButtonInsideTask.style.color='aqua';
   deleteButtonInsideTask.style.fontWeight='bolder';
   deleteButtonInsideTask.style.fontSize='25px';
   deleteButtonInsideTask.style.borderRadius='5px';
